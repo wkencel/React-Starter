d54d0684edf9339de6b3ab51cb1e77f36671cab8
Will Kencel <wkencel1@gmail.com>
2021-04-11 01:05:16 -0400

    initial commit of working starter file

---
+++ src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+      <h3>Here's a react playground with minimal packages.  Just your good old react here along with webpack & babel.  Feel free to check out more of my packages at [link to website here]</h3>
+  );
+};
+
+export default HelloWorld;
